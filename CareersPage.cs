4c41d0b09fc32005ed9e678a8d786c71efa00523
Eremeev <valeriy.eremeev@indeed-id.com>
2019-12-28 19:30:23 +0300

    Добавьте файлы проекта.

---
+++ CareersPage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace ConsoleApp1
+{
+    internal class CareersPage
+    {
+        private IWebDriver driver;
+
+
+        public CareersPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        internal void choseCountry(String country)
+        {
+            driver.FindElement(By.Id("country-element")).Click();
+            ReadOnlyCollection<IWebElement> listOfcountry = driver.FindElements(By.CssSelector("#country-element div.scroller-content span"));
+            foreach (IWebElement element in listOfcountry)
+            {
+                if (element.Text == country)
+                {
+                    element.Click();
+                    break;
+                }
+
+            }
+            //driver.FindElement(By.CssSelector("[data-value='"+country+"']")).Click();
+        }
+
+        internal ReadOnlyCollection<IWebElement> getVacancies()
+        {
+            driver.FindElement(By.CssSelector(".load-more-button")).Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.ElementExists(By.CssSelector(".hide.load-more-button.content-loader-button")));
+            return driver.FindElements(By.CssSelector(".vacancies-blocks-col"));
+        }
+
+        internal void choseLanguage(string language)
+        {
+            driver.FindElement(By.Id("language")).Click();
+            ReadOnlyCollection<IWebElement> listOfLanguage = driver.FindElements(By.CssSelector(".controls-checkbox"));
+            foreach (IWebElement element in listOfLanguage)
+            {
+                if (element.Text == language)
+                {
+                    element.Click();
+                    break;
+                }
+            }
+            driver.FindElement(By.CssSelector("#language div.container a")).Click();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    [TestFixture]
+    public class Program
+    {
+        IWebDriver driver = new ChromeDriver();
+        static void Main(string[] args)
+       { 
+
+       }
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver.Url = "https://careers.veeam.com/";
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Test()
+        {
+            CareersPage careersPage = new CareersPage(driver);
+            careersPage.choseCountry("Romania");
+            careersPage.choseLanguage("English");
+            Assert.AreEqual(26, careersPage.getVacancies().Count);
+        }
+
+        [TearDown]
+        public void CleanUP()
+        {
+            //driver.Close();
+        }
+    }
+}
